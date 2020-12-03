f429f693b6013bb2313d4b2a289ee29e851a06d5
Kpatel114 <31674536+Kpatel114@users.noreply.github.com>
2020-12-03 10:04:45 -0600

    2 new R files that export links and content file

---
+++ Reddit/RedditSubreddit_Comments_contentfile.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(RedditExtractoR)
+
+links <- reddit_urls(
+  search_terms = "al qaeda",
+  cn_threshold = 20,
+  page_threshold = 4
+)
+ 
+max_url <- links %>% filter(num_comments == max(num_comments))
+
+content <- reddit_content(max_url$URL[1], wait_time = 2)
+
+# content <- reddit_content("https://www.reddit.com/r/Python/comments/k2pyeb/recording_terminal_sessions_with_python/")
+write.csv(links, file = "links.csv", row.names = FALSE)
